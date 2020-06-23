744783c70257279be6322e9e0b11d1880e233bc9
Gautam <61274514+Gautam-flash@users.noreply.github.com>
2020-06-23 20:34:53 +0530

    Created folder

---
+++ RCB_BLE/BLE_RGB.ino
@@ -0,0 +1,59 @@
+/*
+  Controlling RGB Lights using Arduino Uno and HM-10
+  connect HM-10 as TX-RX
+               and RX-TX 
+
+  Circuit: Common-Cathode RGB LED wired like so:
+  - red anode: digital pin 3
+  - green anode: digital pin 5
+  - blue anode: digital pin 6
+  - blue anode: digital pin 10
+*/
+
+// pins for the LEDs:
+const int redPin = 3;
+const int greenPin = 5;
+const int bluePin = 6;
+const int yellowpin = 10;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // make the pins outputs:
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(yellowpin, OUTPUT);
+
+}
+
+void loop() {
+  // if there's any serial available, read it:
+  while (Serial.available() > 0) {
+
+    // look for the next valid integer in the incoming serial stream:
+    int red = Serial.parseInt();
+    // do it again:
+    int green = Serial.parseInt();
+    // do it again:
+    int blue = Serial.parseInt();
+    //again
+    int yellow = Serial.parseInt();
+
+    // look for the newline. That's the end of your sentence:
+    if (Serial.read() == '\n') {
+      // constrain the values to 0 - 255 and invert
+      // if you're using a common-cathode LED, just use "constrain(color, 0, 255);"
+      red = 255 - constrain(red, 0, 255);
+      green = 255 - constrain(green, 0, 255);
+      blue = 255 - constrain(blue, 0, 255);
+      yellow = 255 - constrain(yellow, 0, 255);
+
+      // fade the red, green, and blue legs of the LED:
+      analogWrite(redPin, red);
+      analogWrite(greenPin, green);
+      analogWrite(bluePin, blue);
+      analogWrite(yellowpin, yellow);
+    }
+  }
+}
